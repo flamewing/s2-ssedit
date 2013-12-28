1dfaa065c9782b486ad5595f79bcc25701150dab
flamewing.sonic <flamewing.sonic@b300e1b6-c521-9e22-22c9-4fcaed927e9c>
2013-12-28 01:02:49 +0000

    Missing 'm' made KosM short form option in split_art non-functional.

--- trunk/src/tools/split_art.cc
+++ trunk/src/tools/split_art.cc
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
 
 	while (true) {
 		int option_index = 0;
-		int c = getopt_long(argc, argv, "c",
+		int c = getopt_long(argc, argv, "cm",
 		                    long_options, &option_index);
 		if (c == -1)
 			break;
